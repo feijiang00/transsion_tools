71b302974ecd388af4c79a262d293ef1ea83d88b
feijiang00 <2878383634@qq.com>
2024-07-11 09:20:02 +0800

    修改脚本，添加holmes

--- tools/operation/get_all.sh
+++ tools/operation/get_all.sh
@@ -20,15 +20,42 @@ TARGET_SECONDS=$(date -d "1970-01-01 $TARGET_TIME UTC" +%s)
 
 # 定义一个函数来过滤和输出标准时间戳日志
 filter_logs() {
-  local keyword="$1"
-  local label="$2"
-  local before_lines="$3"
-  local after_line="$4"
-  local range_multiplier="$5"
+  local keyword1="$1"
+  local negate1="$2"
+  local keyword2="$3"
+  local negate2="$4"
+  local label="$5"
+  local before_lines="$6"
+  local after_line="$7"
+  local range_multiplier="$8"
+  
   echo -e "\n==================== $label 分割线 ===================="
-  grep "$keyword" "$LOG_DIR" -rni -A "$after_line" -B "$before_lines" | while read -r line; do
+
+  # 构建第一个 grep 命令
+  if [ "$negate1" == "true" ]; then
+    grep_cmd1="grep --binary-files=text -v \"$keyword1\""
+  else
+    grep_cmd1="grep --binary-files=text \"$keyword1\""
+  fi
+
+  # 检查是否传入第二个关键字和其取反参数
+  if [ -n "$keyword2" ]; then
+    if [ "$negate2" == "true" ]; then
+      grep_cmd2="grep --binary-files=text -v \"$keyword2\""
+    else
+      grep_cmd2="grep --binary-files=text \"$keyword2\""
+    fi
+  else
+    grep_cmd2="cat"  # 如果没有传入第二个关键字，直接传递所有行
+  fi
+
+  # 运行组合后的 grep 命令
+  eval "grep -rni --binary-files=text -A \"$after_line\" -B \"$before_lines\" \"$LOG_DIR\" | $grep_cmd1 | $grep_cmd2" | while read -r line; do
+    # 调试 打印line 
+    # echo "$line"
+
     # 提取标准时间戳部分 (例如 22:36:01.436184)
-    TIMESTAMP=$(echo "$line" | grep -oP '\d{2}:\d{2}:\d{2}\.\d{6}' | cut -d'.' -f1)
+    TIMESTAMP=$(echo "$line" | grep -oP '\d{2}:\d{2}:\d{2}\.\d{3,6}' | cut -d'.' -f1)
 
     if [ -n "$TIMESTAMP" ]; then
       # 当前行时间转换为秒
@@ -46,25 +73,28 @@ filter_logs() {
   done
 }
 
-# 参数含义： 过滤关键字 tit标题 向前多少行 向后多少行 倍数范围(搭配调用时倍数相乘)
+# 参数含义： 过滤关键字1 是否反向过滤1 过滤关键字2(可选) 是否反向过滤2(可选) 标题 向前多少行 向后多少行 倍数范围
 
 # 过滤anr日志
-filter_logs "am_anr" "ANR" 0 0 10
+filter_logs "am_anr" false "" "" "ANR" 0 0 10
 
 # 过滤crash 日志
-filter_logs "am_crash" "crash" 0 0 10
+filter_logs "am_crash" false "" "" "crash" 0 0 10
 
 # 过滤 am_proc_start 日志
-filter_logs "am_proc_start" "自启" 0 0 10
+filter_logs "am_proc_start" false "next-top-activity" true "应用自启" 0 0 10
+
+# 过滤 am_proc_start 日志(用户收到启动)
+filter_logs "am_proc_start" false "next-top-activity" false "用户手起" 0 0 10
 
 # 过滤 lowmemorykiller: Kill 日志
-filter_logs "lowmemorykiller: Kill" "低内存查杀" 0 0 10
+filter_logs "lowmemorykiller: Kill" false "" "" "低内存查杀" 0 0 10
 
 # 过滤 SKIN 日志
-filter_logs "SKIN" "板温" 0 0 2
+filter_logs "SKIN" false "" "" "板温" 0 0 2
 
 # 过滤 thermal_core 日志
-filter_logs "thermal_core" "限频" 0 0 10
+filter_logs "thermal_core" false "" "" "限频" 0 0 10
 
 
 
@@ -109,7 +139,7 @@ filter_tran_perf_logs() {
         line_num=$(echo "$line" | cut -d: -f2)
 
         # 输出匹配行及其后的7行
-        sed -n "${line_num},$((line_num + 7))p" "$file"
+        sed -n "${line_num},$((line_num + 8))p" "$file"
       fi
     done
   done
